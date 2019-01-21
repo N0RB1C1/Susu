42413b07920087da61d0a4a7283c7d144bae57a4
N0RB1C1 <gilanyi.norb@gmail.com>
2019-01-21 14:35:34 +0100

    Exit button

--- Zarodoga/Menu.Designer.cs
+++ Zarodoga/Menu.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Exit_Button = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Exit_Button
+            // 
+            this.Exit_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Exit_Button.Location = new System.Drawing.Point(224, 449);
+            this.Exit_Button.Name = "Exit_Button";
+            this.Exit_Button.Size = new System.Drawing.Size(165, 49);
+            this.Exit_Button.TabIndex = 0;
+            this.Exit_Button.Text = "Exit";
+            this.Exit_Button.UseVisualStyleBackColor = true;
+            this.Exit_Button.Click += new System.EventHandler(this.Exit_Button_Click);
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(610, 594);
+            this.Controls.Add(this.Exit_Button);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Menu";
@@ -45,5 +58,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Exit_Button;
     }
 }
--- Zarodoga/Menu.cs
+++ Zarodoga/Menu.cs
@@ -16,5 +16,11 @@ namespace Zarodoga
         {
             InitializeComponent();
         }
+
+        // Alkalmazás bezárása
+        private void Exit_Button_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
